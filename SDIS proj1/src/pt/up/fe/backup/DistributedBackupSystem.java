edf7f395f55acbd85342550545787e9197cb0949
Guntler <pbpdirector@hotmail.com>
2014-03-22 20:08:55 +0000

    Testing merge

--- SDIS proj1/src/pt/up/fe/backup/DistributedBackupSystem.java
+++ SDIS proj1/src/pt/up/fe/backup/DistributedBackupSystem.java
@@ -1,25 +1,41 @@
 package pt.up.fe.backup;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class DistributedBackupSystem {
 	private FileManager fManager;
 	private TaskManager tManager;
 	private PacketListener listener;
 	
+	protected static MulticastSocket socket = null;
+	
+	protected static String[] mcastArgs;
+	
 	/**
 	 * @param args	<IPMC> <portMC> <ipMDB> <portMDB> <ipMDR> <portMDR> 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		if (args.length != 6) {
 			System.out.println("Usage: java DistributedBackupSystem <IPMC> <portMC> <ipMDB> <portMDB> <ipMDR> <portMDR>");
 			return;
 		}
+		mcastArgs = args;
 		
 		DistributedBackupSystem system = new DistributedBackupSystem();
 		system.start();
 	}
 	
-	public void start() {
+	public void start() throws IOException {
 		listener = new PacketListener();
 		listener.run();
+		
+		final int mCastPort = Integer.parseInt(mcastArgs[1]);
+		
+		socket = new MulticastSocket(mCastPort);
+		InetAddress group = InetAddress.getByName(mcastArgs[0]);
+		socket.joinGroup(group);
 	}
 }
